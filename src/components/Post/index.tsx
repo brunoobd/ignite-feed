ec652d8c60662af9f8fbf061ae45d90935ef93d4
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-03 16:46:24 -0300

    feat: add Post component

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,41 @@
+import styles from "./styles.module.css";
+
+export const Post = () => {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/brunoobd.png"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Bruno Duarte</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifólio. É um projeto que
+          fiz no NLW Return, evento da Rocketseat. O nome do projeto é
+          DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+};
