7f99ae90ee6f721e6e1d9f12dce0d209c22b3e5a
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-03 14:12:37 -0300

    feat: add app.modules.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Header } from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
+
+import { Sidebar } from "./components/Sidebar";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
     </>
   );
 }
