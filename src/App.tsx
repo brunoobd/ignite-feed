8cd39b4536b356fa35a634f785b4ac8c5271314c
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-03 13:43:09 -0300

    feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Header } from "./components/Header";
 import "./global.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
