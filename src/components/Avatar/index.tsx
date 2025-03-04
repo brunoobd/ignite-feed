1f18ba51346f0719f94cc11f3d9f4754e54efba2
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-04 19:26:03 -0300

    refactor: adjust Avatar component props

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,15 +1,13 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./styles.module.css";
 
-type Props = {
-  src: string;
+type Props = ImgHTMLAttributes<HTMLImageElement> & {
   hasBorder?: boolean;
 };
 
-export function Avatar({ src, hasBorder = true }: Props) {
-  return (
-    <img
-      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-    />
-  );
-}
+export const Avatar = ({ hasBorder = true, ...props }: Props) => (
+  <img
+    className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+    {...props}
+  />
+);
